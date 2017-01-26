e3003d129700e7db575f8f1768996f7cad72beae
bogil-Gong <bogil2006@gmail.com>
2017-01-26 14:59:46 +0900

    Add Edit and Update and Destroy

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -2,7 +2,7 @@ class PicsController < ApplicationController
   before_action :find_pic, only: [:show, :edit, :update, :destroy]
 
   def index
-
+    @pics = Pic.all.order("created_at DESC")
   end
 
   def show
@@ -23,6 +23,22 @@ class PicsController < ApplicationController
     end
   end
 
+  def edit
+
+  end
+
+  def update
+    if @pic.update(pic_params)
+      redirect_to @pic, notice: "Congrates! Pic was update!"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @pic.destroy
+    redirect_to root_path
+  end
 
   private
 
